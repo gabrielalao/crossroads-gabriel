d4f65628520dba4cee3e5b20ec92f90670754b59
gabrielalao <gabrieldeveloper1@gmail.com>
2024-04-08 05:21:52 -0500

    add configuration

---
+++ src/constants/config.ts
@@ -0,0 +1,13 @@
+interface iConfig {
+  GITHUB_OWNER: string;
+  GITHUB_REPO: string;
+  API_VERSION: string;
+  PAGE_LIMIT: number;
+}
+
+export const configuration: iConfig = {
+  GITHUB_OWNER: "gabrielalao",
+  GITHUB_REPO: "crossroads-gabriel",
+  API_VERSION: "2022-11-28",
+  PAGE_LIMIT: 30,
+};
