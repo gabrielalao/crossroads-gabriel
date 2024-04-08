e874431126cefa69029ba6b53ca2f5864d972afb
gabrielalao <gabrieldeveloper1@gmail.com>
2024-04-08 06:56:07 -0500

    add component - CommitListElement

---
+++ src/components/CommitListElement.tsx
@@ -0,0 +1,43 @@
+import { DEFAULT_AVATAR } from "../constants/strings";
+import { GithubCommit } from "../types/commit.type";
+
+interface Props {
+  commit: GithubCommit;
+  owner: string;
+  repo: string;
+}
+const CommitListElement: React.FC<Props> = ({ commit, owner, repo }) => {
+  const sha = commit.sha.substring(0, 7);
+  return (
+    <div key={commit.sha} className="flex mb-2">
+      <a
+        href={
+          commit.author ? `https://github.com/${commit.author.login}` : undefined
+        }
+      >
+        <img
+          className="w-8 h-8 rounded-full"
+          src={commit.author?.avatar_url ?? DEFAULT_AVATAR}
+          alt={commit.author?.login}
+        />
+      </a>
+      <div className="ml-4">
+        <div className="text-sm flex gap-4">
+          <span className="font-medium">{commit.commit.message}</span>
+          <a
+            href={`https://github.com/${owner}/${repo}/commit/${sha}`}
+            className="text-blue-500 hover:underline"
+          >
+            {commit.sha.substring(0, 7)}
+          </a>
+        </div>
+        <div className="text-xs text-gray-500">
+          <span>{commit.commit.author.name}</span>
+          <span>{commit.commit.author.date}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommitListElement;
